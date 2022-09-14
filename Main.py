6db870a479ee64307629834329d395368429707d
Ebin Alex <ebinalex@karunya.edu.in>
2022-09-14 11:12:07 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 class Node:
-    def __init__(self, data=None):
+    def _init_(self, data=None):
         self.data = data
         self.previous = self
         self.next = self
 
+        
 
 class DoublyCircularLinkedList:
-    def __init__(self):
+    def _init_(self):
         self.head = None
         self.count = 0
 
@@ -115,3 +116,19 @@ for item in input_data.split(', '):
             if letter.isnumeric():
                 inner_list.append(int(letter))
         data.append(inner_list)
+obj = DoublyCircularLinkedList()
+result = []
+for i in range(len(operations)):
+    if operations[i] == "add_at_head":
+        result.append(obj.add_at_head(data[i]))
+    elif operations[i] == "add_at_tail":
+        result.append(obj.add_at_tail(data[i]))
+    elif operations[i] == "add_at_index":
+        result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
+    elif operations[i] == "get":
+        result.append(obj.get(data[i]))
+    elif operations[i] == "get_previous_next":
+        result.append(obj.get_previous_next(data[i]))
+    elif operations[i] == 'delete_at_index':
+        result.append(obj.delete_at_index(data[i]))
+print(result)
